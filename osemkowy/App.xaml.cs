c4f7fb31abdd0e3731f01668962aff50d24a2cf9
Sala 119 <Sala 119@120SALA>
2024-09-16 12:12:55 +0200

    Dodaj pliki projektów.

---
+++ osemkowy/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace osemkowy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ osemkowy/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace osemkowy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            int licz;
+            String wyn= "";
+
+
+            if (Int32.TryParse(liczba.Text, out licz))
+            {
+                while(licz != 0)
+                {
+                    wyn = Convert.ToString(licz % 8) + wyn;
+                    licz = licz / 8;        
+                }
+
+                wynik.Content = wyn;
+
+            }
+            else
+            {
+                MessageBox.Show("Błąd");
+            }
+
+            
+
+        }
+    }
+}
